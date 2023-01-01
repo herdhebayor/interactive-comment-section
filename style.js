484b5c6d544a028c055d0191f85f3cce76cd50f7
herdhebayor <98285362+herdhebayor@users.noreply.github.com>
2023-01-01 09:41:30 +0100

    added score function

--- style.js
+++ style.js
@@ -6,3 +6,27 @@ const thirdComment = document.getElementById("user3comm").innerHTML = data.comme
 const fourthComment = document.getElementById("user4comm").innerHTML = data.comments[1].replies[1].content;
 
 // TO EDIT COMMENT
+const plus = document.querySelectorAll(".plus");
+const minus = document.querySelectorAll(".minus");
+plus.forEach((items)=>{
+    items.addEventListener('click', (e)=>{
+        let btnParent = e.target.parentElement;
+        let score = btnParent.nextElementSibling
+        let x = score.innerHTML;
+        x++;
+        score.innerHTML = x;
+        if(score.innerHTML ===20){
+            clearInterval();
+            return
+        }
+    })
+})
+minus.forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+        let btnParent = e.target.parentElement;
+        let val = btnParent.previousElementSibling
+        let x = val.innerHTML;
+        x--;
+        val.innerHTML = x;
+    })
+})
