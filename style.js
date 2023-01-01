5a8dab5fe678ed239497dbd5edcd613b01ca1417
herdhebayor <98285362+herdhebayor@users.noreply.github.com>
2023-01-01 22:42:05 +0100

    added some function

--- style.js
+++ style.js
@@ -1,6 +1,6 @@
 import data from './data.json' assert {type:'json'};
 console.log(data.comments[0]);
-const firstComment = document.getElementById("user1comm").innerHTML = data.comments[0].content;
+const firstComment = document.getElementById("user1comm").innerHTML =  data.comments[0].content;
 const secondComment = document.getElementById("user2comm").innerHTML= data.comments[1].content;
 const thirdComment = document.getElementById("user3comm").innerHTML = data.comments[1].replies[0].content;
 const fourthComment = document.getElementById("user4comm").innerHTML = data.comments[1].replies[1].content;
@@ -8,6 +8,8 @@ const fourthComment = document.getElementById("user4comm").innerHTML = data.comm
 // TO EDIT COMMENT
 const plus = document.querySelectorAll(".plus");
 const minus = document.querySelectorAll(".minus");
+
+// score function
 plus.forEach((items)=>{
     items.addEventListener('click', (e)=>{
         let btnParent = e.target.parentElement;
@@ -15,10 +17,6 @@ plus.forEach((items)=>{
         let x = score.innerHTML;
         x++;
         score.innerHTML = x;
-        if(score.innerHTML ===20){
-            clearInterval();
-            return
-        }
     })
 })
 minus.forEach((item)=>{
@@ -30,3 +28,30 @@ minus.forEach((item)=>{
         val.innerHTML = x;
     })
 })
+
+const btnReply = document.querySelectorAll(".reply");
+    btnReply.forEach((item)=>{
+        item.addEventListener('click',(e)=>{
+            let el = e.target.parentElement;
+            let parent = el.parentElement; 
+        let box = document.getElementById("curCom");
+        let btn = document.getElementById("submit");
+        parent.appendChild(box);
+        let txt = document.getElementById("textarea");
+         txt.focus();
+        btn.innerHTML = "post";
+        btn.addEventListener('click',()=>{
+            let wrapper = document.createElement("div");
+            wrapper.classList.add("wrapper");
+            let replyBody = document.getElementById("newComment");
+            wrapper.appendChild(replyBody);
+            
+            replyBody.style.display = "block";
+            parent.appendChild(wrapper);
+            parent.removeChild(box);
+
+        })
+       
+            })
+    })
+
